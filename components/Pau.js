3ed45a9e4bfd32884ca25d0bdfacfa747cecd6c7
dvdapogeo <dvdingles@hotmail.com>
2022-10-17 09:31:05 +0200

    first

---
+++ components/Pau.js
@@ -0,0 +1,20 @@
+
+import React from "react";
+
+export function Pau() {
+
+
+  return (
+
+    <>
+        <video width={'90%'} height={'50%'} autoPlay muted controls>
+            <source src={'../public/aaa.mp4'} type={'video/mp4'}>
+              </source>
+        </video>
+    </>
+    
+  );
+}
+
+
+
